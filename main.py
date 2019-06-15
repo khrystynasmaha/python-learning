507117d26507aa9f70706be2e2538224e1802366
khrystyna-smaha <xrustia.smaga@gmail.com>
2019-06-15 16:32:43 -0700

    added the exercise on swapping out the first and second parts of the string

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
-# Ask the user for a number and then print the area of a circle with that number as the radius.
+# Ask the user for a number and then print
+# the area of a circle with that number as the radius.
 
 from math import pi
 r=input("What is a number?")
 print(pi*int(r)**2)
 
-# Write a program that forms and prints a new string by exchanging the first and last characters in the string "mystery".
+# Write a program that forms and prints a new string by exchanging
+# the first and last characters in the string "mystery".
 # V-1
 
 last=(len('mystery')-1)
@@ -17,3 +19,14 @@ print('mystery'[-1] + \
       'mystery'[1:math.floor(len('mystery')/2)] + \
       'mystery'[math.floor(len('mystery')/2):6] + \
                                               'mystery'[0])
+
+# Ask the user for an input string of length at least two
+# and print the string swapping out the first and second parts of it
+# where second half is in upper case and the first half is in lower case.
+
+from math import floor
+name=input("Name a string with at least two characters")
+mid=(floor(len(name)/2))
+first_part = name[0:mid]
+second_part = name[mid:]
+print(second_part.lower()+first_part.upper())
